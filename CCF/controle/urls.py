df824acbb1d33311732229d08f5362164acb0b66
pedro <pedrofrs008@gmail.com>
2024-05-12 21:20:22 -0300

    novo: Adicionando Modal Add Cliente

--- CCF/controle/urls.py
+++ CCF/controle/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('register/', views.register_view, name='register'),
     path('clientes/', views.clientes_view, name='clientes'),
     path('fantasias/', views.fantasias_view, name='fantasias'),
+    path('historico/', views.historico_view, name='historico'),
     path('', views.index, name='index'),
 ] 
